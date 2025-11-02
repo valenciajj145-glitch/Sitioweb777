a1755625c4e35e65044c8bb451610a708a800a05
valenciajj145-glitch <valenciajj145@gmail.com>
2025-11-02 15:19:19 -0500

    Update login.js

--- api/login.js
+++ api/login.js
@@ -1,7 +1,10 @@
-export default function handler(req, res) {
-  const clientId = process.env.DISCORD_CLIENT_ID;
-  const redirectUri = encodeURIComponent(`${process.env.BASE_URL}/api/callback`);
-  const scope = "identify";
-  const url = `https://discord.com/api/oauth2/authorize?client_id=${clientId}&redirect_uri=${redirectUri}&response_type=code&scope=${scope}`;
-  res.redirect(url);
-}
+const redirectUri = process.env.DISCORD_REDIRECT_URI;
+
+const params = new URLSearchParams({
+  client_id: process.env.DISCORD_CLIENT_ID,
+  redirect_uri: redirectUri,
+  response_type: "code",
+  scope: "identify",
+});
+
+res.redirect(`https://discord.com/oauth2/authorize?${params.toString()}`);
